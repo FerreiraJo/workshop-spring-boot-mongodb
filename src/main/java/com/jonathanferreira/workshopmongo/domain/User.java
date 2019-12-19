cafbfa69341e3b57d5b8e9b3c50e2017ad65a9af
FerreiraJo <jonathan_silva02@hotmail.com>
2019-12-19 15:11:02 -0300

    Referenciando os Posts do usuarios

--- src/main/java/com/jonathanferreira/workshopmongo/domain/User.java
+++ src/main/java/com/jonathanferreira/workshopmongo/domain/User.java
@@ -1,9 +1,13 @@
 package com.jonathanferreira.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 
 @Document(collection="user") /* indica será uma coleção do MongoDB */
 public class User implements Serializable {
@@ -14,40 +18,51 @@ public class User implements Serializable {
 	private String name;
 	private String email;
 	
+	@DBRef(lazy = true) /* garante que os posts so serao carregados se forem acessados */
+	private List<Post> posts = new ArrayList<>();
+	
 	public User() {
 	}
-
+	
 	public User(String id, String name, String email) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 	}
-
+	
 	public String getId() {
 		return id;
 	}
-
+	
 	public void setId(String id) {
 		this.id = id;
 	}
-
+	
 	public String getName() {
 		return name;
 	}
-
+	
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	
 	public String getEmail() {
 		return email;
 	}
-
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+	
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -55,7 +70,7 @@ public class User implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
